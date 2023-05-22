763d4e3c3b6f25fb2ff4c7debe0446edbe8952d7
Sofia Di Blasi <phia.di.blasi@gmail.com>
2023-05-22 21:28:20 +0100

    Added props

---
+++ src/components/AppCatalogoItem.vue
@@ -0,0 +1,24 @@
+<script>
+    export default {
+        props: {
+            "thumb": String,
+            "price": String,
+            "series": String,
+            "type": String,
+        }
+    }
+</script>
+
+<template>
+    <div>
+        <img :src="thumb" alt="" width="150" height="150">
+        <p class="textCatalogo">{{series}}</p>
+    </div>
+</template>
+
+<style>
+.textCatalogo {
+    color: white;
+    font-size: 8px;
+}
+</style>
